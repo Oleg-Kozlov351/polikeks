c541b1fa2e36c06e87ae10f3212cb429008cfcb8
temp <temp@STUD.UNV.SU>
2018-09-22 13:36:20 +0300

    Some comment

---
+++ autocommit.bat
@@ -0,0 +1,6 @@
+git add *.cpp *.h *.sln *.vcxproj *.user *.filters *.bat
+git commit -am "Some comment"
+git status
+git log
+git remote add Oleg-Kozlov351 https://github.com/Oleg-Kozlov351/polikeks
+pause
